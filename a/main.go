c93876bae18b5feea8e18a32da5c7fe759a2ea94
smith-30 <kohei.misu@techtouch.co.jp>
2020-06-26 00:04:49 +0900

    mod:

--- a/main.go
+++ a/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/smith-30/cidind/rdb"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -10,6 +12,10 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	_, err := rdb.Mysql("root", "MYSQL_ADMIN", "mysql", "3306", "testdb", true)
+	if err != nil {
+		panic(err)
+	}
 	http.HandleFunc("/", handler) // ハンドラを登録してウェブページを表示させる
 	http.ListenAndServe("0.0.0.0:8011", nil)
 }
---
+++ rdb/conn.go
@@ -0,0 +1,17 @@
+package rdb
+
+import (
+	"fmt"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+func Mysql(username, password, host, port, dbName string, logging bool) (*gorm.DB, error) {
+	address := host + ":" + port
+	setting := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8&parseTime=True&loc=Local", username, password, address, dbName)
+	db, err := gorm.Open("mysql", setting)
+	db.LogMode(logging)
+
+	return db, err
+}
