7d0ef533fbd79faa84ee398ead30ba2290781ffb
smith-30 <kohei.misu@techtouch.co.jp>
2020-06-27 23:51:44 +0900

    mod:

--- rdb/conn.go
+++ rdb/conn.go
@@ -10,6 +10,7 @@ import (
 func Mysql(username, password, host, port, dbName string, logging bool) (*gorm.DB, error) {
 	address := host + ":" + port
 	setting := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8&parseTime=True&loc=Local", username, password, address, dbName)
+	fmt.Printf("setting %#v\n", setting)
 	db, err := gorm.Open("mysql", setting)
 	db.LogMode(logging)
 
